3a1d88215db9ccf13308b9a41380e675d8f332af
audrbar <audrbar@gmail.com>
2023-11-04 18:06:40 +0200

    editor

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Button, TextField } from '@radix-ui/themes';
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
+
+const NewIssuePage = () => {
+  return (
+    <div className='max-w-xl space-y-3'>
+        <TextField.Root>
+            <TextField.Input placeholder="Title" />
+        </TextField.Root>
+        <SimpleMDE placeholder="Description…" />
+        <Button>Submit New Issue</Button>
+    </div>
+  );
+}
+
+export default NewIssuePage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import '@radix-ui/themes/styles.css';
+import './theme-config.css';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Theme } from '@radix-ui/themes';
+import { Theme, ThemePanel } from '@radix-ui/themes';
 import NavBar from './NavBar';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,12 +23,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+      <body className={inter.variable}>
+        <Theme accentColor="gray" grayColor="mauve">
           <NavBar />
-          <main>
-            {children}
-          </main>
+          <main className='p-5'>{children}</main>
         </Theme>
       </body>
     </html>
