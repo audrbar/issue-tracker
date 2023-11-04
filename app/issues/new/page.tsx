15b887029c23d9283863b0deb57a8eeb473de3ad
audrbar <audrbar@gmail.com>
2023-11-04 20:20:11 +0200

    spiner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,8 @@ import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spiner';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -19,6 +21,7 @@ const NewIssuePage = () => {
         resolver: zodResolver(createIssueSchema)
     });
     const [error, setError] = useState('');
+    const [isSubmitting, setSubmitting] = useState(false);
   return (
     <div className='max-w-xl space-y-3'>
         {error && <Callout.Root color="red">
@@ -30,9 +33,11 @@ const NewIssuePage = () => {
             className='max-w-xl space-y-3'
             onSubmit={handleSubmit(async(data) => {
                 try {
+                    setSubmitting(true);
                     await axios.post('/api/issues', data);
                     router.push('issues');
                 } catch (error) {
+                    setSubmitting(false);
                     setError('An unespected error occured.')
                 }
 
@@ -40,14 +45,18 @@ const NewIssuePage = () => {
             <TextField.Root>
                 <TextField.Input placeholder="Title" {...register('title')} />
             </TextField.Root>
-            {errors.title && <Text color='red' as='p'>{errors.title.message}</Text>}
+            <ErrorMessage>
+                {errors.title?.message}
+            </ErrorMessage>
             <Controller
                 name='description'
                 control={control}
                 render={({ field }) => <SimpleMDE placeholder="Description…" {...field} /> }
             />
-            {errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
-            <Button>Submit</Button>
+            <ErrorMessage>
+                {errors.description?.message}
+            </ErrorMessage>
+            <Button disabled={isSubmitting}>Submit {isSubmitting && <Spinner />}</Button>
         </form>
     </div>
   );
