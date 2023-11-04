21e1bd51b0215b95711b9199fbb83b2092c2f0df
audrbar <audrbar@gmail.com>
2023-11-04 18:50:47 +0200

    error-handle

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -6,8 +6,8 @@ import prisma from '@/prisma/client';
 // import authOptions from '@/app/auth/authOptions';
 
 const createIssueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1)
+    title: z.string().min(1, 'Title is required').max(255),
+    description: z.string().min(1, 'Description is required')
 });
 
 export async function POST(request: NextRequest) {
