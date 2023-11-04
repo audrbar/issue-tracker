8cc04d39eecb5abe5144b8e81ac8819a58cbce1d
audrbar <audrbar@gmail.com>
2023-11-04 17:33:24 +0200

    radix-ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Button } from '@radix-ui/themes';
 
 const IssuesPage = () => {
   return (
     <div className='p-12'>
-        Issues
+        <Button>New Issue</Button>
     </div>
   );
 }
