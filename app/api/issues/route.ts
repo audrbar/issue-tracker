6784df51c5bc70d9e7e26d786a4bb03f69660942
audrbar <audrbar@gmail.com>
2023-11-04 17:23:49 +0200

    create_api

---
+++ app/api/issues/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+import z from 'zod';
+import prisma from '@/prisma/client';
+// import { issueSchema } from '../../validationSchemas';
+// import { getServerSession } from 'next-auth';
+// import authOptions from '@/app/auth/authOptions';
+
+const createIssueSchema = z.object({
+    title: z.string().min(1).max(255),
+    description: z.string().min(1)
+});
+
+export async function POST(request: NextRequest) {
+    // const session = await getServerSession(authOptions);
+    // if (!session)
+    //     return NextResponse.json({}, { status: 401 });
+
+    const body = await request.json();
+    const validation = createIssueSchema.safeParse(body);
+    if (!validation.success)
+        return NextResponse.json(validation.error.format(), { status: 400 });
+
+    const newIssue = await prisma.issue.create({
+        data: { title: body.title, description: body.description },
+    });
+
+    return NextResponse.json(newIssue, { status: 201 });
+}
