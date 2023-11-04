5a4a5065b15464900abeefe681457c54091c36e0
audrbar <audrbar@gmail.com>
2023-11-04 21:18:50 +0200

    components

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,36 +1,102 @@
 "use client";
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import React from 'react';
-import { AiFillBug } from 'react-icons/ai';
-import classnames from 'classnames';
+import { Skeleton } from "@/app/components";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
+import { useSession } from "next-auth/react";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 const NavBar = () => {
-    const currentPath = usePathname();
-
-    const links = [
-        { label: 'Dashboard', href: '/' },
-        { label: 'Issues', href: '/issues' },
-    ];
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/"><AiFillBug /></Link>
-        <ul className='flex space-x-6'>
-            {links.map(link =>
-            <Link
-                key={link.href}
-                className={classnames({
-                    'text-zinc-900': link.href === currentPath,
-                    'text-zinc-500': link.href !== currentPath,
-                    'hover:text-zinc-800 transition-colors': true
-                })}
-                href={link.href}>
-                {link.label}
-            </Link>)}
-        </ul>
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <AiFillBug />
+            </Link>
+            <NavLinks />
+          </Flex>
+          <AuthStatus />
+        </Flex>
+      </Container>
     </nav>
   );
-}
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues/list" },
+  ];
+
+  return (
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classnames({
+              "nav-link": true,
+              "!text-zinc-900": link.href === currentPath,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === "loading") return <Skeleton width="3rem" />;
+
+  if (status === "unauthenticated")
+    return (
+      <Link className="nav-link" href="/api/auth/signin">
+        Login
+      </Link>
+    );
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size="2">{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href="/api/auth/signout">Log out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
 
 export default NavBar;
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,9 +1,28 @@
-import z from 'zod';
+import { z } from "zod";
 
-// import { issueSchema } from '../../validationSchemas';
-// import { getServerSession } from 'next-auth';
-// import authOptions from '@/app/auth/authOptions';
-export const createIssueSchema = z.object({
-    title: z.string().min(1, 'Title is required.').max(255),
-    description: z.string().min(1, 'Description is required.')
+export const issueSchema = z.object({
+    title: z.string().min(1, "Title is required.").max(255),
+    description: z
+        .string()
+        .min(1, "Description is required.")
+        .max(65535),
+});
+
+export const patchIssueSchema = z.object({
+    title: z
+        .string()
+        .min(1, "Title is required.")
+        .max(255)
+        .optional(),
+    description: z
+        .string()
+        .min(1, "Description is required.")
+        .max(65535)
+        .optional(),
+    assignedToUserId: z
+        .string()
+        .min(1, "AssignedToUserId is required.")
+        .max(255)
+        .optional()
+        .nullable(),
 });
